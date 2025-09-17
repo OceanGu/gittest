fad1de796f5a2e0f31405a0140961e53d7562b32
rrhdyh <2796449439@qq.com>
2025-09-17 20:37:40 +0800

    微头条1 2st 提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("master1 = " + args);
+        System.out.println("master1111 = " + args);
     }
 }

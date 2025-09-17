5abf89231ecebf25bf5d6d40a20256a14d00cb78
OceanGu <88011422+OceanGu@users.noreply.github.com>
2025-09-17 20:58:54 +0800

    Update Main.java
    pull test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;public class Main {
         System.out.println("Hello world!");
         System.out.println("master = " + args);
         System.out.println("master1111 = " + args);
+        System.out.println("pull = " + args);
     }
 }
